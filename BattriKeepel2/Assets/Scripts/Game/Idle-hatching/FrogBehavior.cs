f134978d723749457b1c04988dca4b8c9cd72e08
Porolito <134312856+Porolito@users.noreply.github.com>
2025-01-16 21:34:36 +0100

    feat : save, not working as intended
    Did push for saving work but movement of frogs doesn't work as intended

--- BattriKeepel2/Assets/Scripts/Game/Idle-hatching/FrogBehavior.cs
+++ BattriKeepel2/Assets/Scripts/Game/Idle-hatching/FrogBehavior.cs
@@ -1,16 +1,65 @@
+using System.Collections;
 using UnityEngine;
 
 public class FrogBehavior : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Transform m_transform;
+    [SerializeField] private Transform m_LeapPosition;
+
+    private Vector3 m_position;
+
+    private float m_waitTime;
+
+    private bool m_isRotating;
+    private bool m_isMoving;
+
+    private Quaternion m_targetRotation;
+
+    private void Start()
     {
-        
+        Vector3 newDir = SearchNewDirection();
+        ExecuteLeap(newDir);
+        StartCoroutine(WaitBetweenLeaps(5f));
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (m_isRotating)
+        {
+            m_transform.rotation = Quaternion.Lerp(m_transform.rotation, m_targetRotation, Time.deltaTime);
+            if(m_transform.rotation == m_targetRotation)
+            {
+                m_isRotating = false;
+                m_isMoving = true;
+            }
+        }
+        else if (m_isMoving) 
+        {
+            m_transform.position = Vector3.Lerp(m_transform.position, m_LeapPosition.position, Time.deltaTime);
+
+        }
+    }
+
+    Vector3 SearchNewDirection()
+    {
+        Vector3 newDirection = Random.insideUnitCircle.normalized;
+        float targetAngle = Mathf.Atan2(newDirection.x, newDirection.y) * Mathf.Rad2Deg; //merci Loutre
+        m_targetRotation = Quaternion.Euler(0, 0, -targetAngle);
+        //m_transform.rotation = Quaternion.Euler(0, 0, -targetAngle);
+        m_isRotating = true;
+        return newDirection;
+    }
+
+    void ExecuteLeap(Vector2 direction)
+    {
+        //m_transform.position = Vector3.Lerp(m_transform.position, m_LeapPosition.position, Time.deltaTime);
+    }
+
+    IEnumerator WaitBetweenLeaps(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        Vector3 newDir = SearchNewDirection();
+        ExecuteLeap(newDir);
+        StartCoroutine(WaitBetweenLeaps(1f));
     }
 }
