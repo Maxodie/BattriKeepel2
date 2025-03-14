0edf8e87ae72b48b57d51717f7b81534330ca650
Maximilien <40921999+Maxodie@users.noreply.github.com>
2025-03-14 10:47:57 +0100

    feat : SaveSystem interface

---
+++ BattriKeepel2/Assets/Scripts/Systems/Save/SaveSystem.cs
@@ -0,0 +1,12 @@
+public static class SaveSystem
+{
+    public static Save(DynamicScriptableObject dynamicData)
+    {
+
+    }
+
+    public static Load(DynamicScriptableObject dynamicData)
+    {
+
+    }
+}
