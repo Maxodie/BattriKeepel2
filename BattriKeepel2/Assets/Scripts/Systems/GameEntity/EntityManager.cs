68c7d4a30734fc6220d316848f7ca61b1b6c20ca
Maximilien <40921999+Maxodie@users.noreply.github.com>
2025-04-08 18:54:00 +0200

    feat : entityManager only for persistent data

--- BattriKeepel2/Assets/Scripts/Systems/GameEntity/EntityManager.cs
+++ BattriKeepel2/Assets/Scripts/Systems/GameEntity/EntityManager.cs
@@ -1,22 +1,29 @@
 using System.Collections.Generic;
+using UnityEngine;
 using System;
 using UnityEngine.Events;
 
 using EntityID = System.Int32;
 
+class EntityManagerLogger : Logger
+{
+
+}
+
 class EntityManager
 {
     UnityEvent<IGameEntity> OnEntityAddedCallback;
+    UnityEvent<GameEntityGraphics> OnVisualCreatedCallback;
     UnityEvent<Type> OnEntityDestroyedCallback;
     List<IGameEntity> m_loadedEntities = new();
-    List<EntityID> m_loadedEntityIDs = new();
-    uint persistentDataOffset = 0;
+    /*List<EntityID> m_loadedEntityIDs = new();*/
+    /*int persistentDataOffset = 0;*/
     List<bool> m_loadedEntitiesActiveState = new();
-    EntityID s_lastID = 0;
+    /*EntityID s_lastID = 0;*/
 
     static EntityManager s_entityManager;
 
-    public static EntityManager GetInstance()
+    public static EntityManager Get()
     {
         if(s_entityManager == null)
         {
@@ -28,79 +35,100 @@ class EntityManager
 
     public EntityID CreatePersistentEntity<TEntity>(out TEntity outEntity, params object[] variadicArgs) where TEntity : IGameEntity
     {
-        persistentDataOffset++;
-        int deletedEntityID = -1;
+        int entityID = m_loadedEntities.FindIndex((IGameEntity entity) => { return entity == null;});
 
-        for(int i = 0; i < persistentDataOffset; i++)
-        {
-            if(m_loadedEntitiesActiveState[i])
-            {
-                deletedEntityID = i;
-                break;
-            }
-        }
-
-        if(deletedEntityID == -1)
-        {
-            outEntity = (TEntity)Activator.CreateInstance(typeof(TEntity), args:variadicArgs);
-            m_loadedEntities.Insert(deletedEntityID, outEntity);
-            m_loadedEntityIDs.Insert(deletedEntityID, s_lastID++);
-            m_loadedEntitiesActiveState.Insert(deletedEntityID, true);
-            return deletedEntityID;
-        }
-        else
-        {
-            return CreateEntityWithID<TEntity>(deletedEntityID, out outEntity);
-        }
-    }
-
-    public EntityID CreateEntity<TEntity>(out TEntity outEntity, params object[] variadicArgs) where TEntity : IGameEntity
-    {
-        int deletedEntityIndex = m_loadedEntitiesActiveState.FindIndex((bool state) => { return state == false;});
-        for(int i=0; i < m_loadedEntitiesActiveState.Count; i++)
-        {
-            if(i >= persistentDataOffset && m_loadedEntitiesActiveState[i])
-            {
-                deletedEntityIndex = i;
-                break;
-            }
-        }
-
-        return CreateEntityWithID<TEntity>(deletedEntityIndex, out outEntity, variadicArgs);
-    }
-
-    private EntityID CreateEntityWithID<TEntity>(int entityID, out TEntity outEntity, params object[] variadicArgs) where TEntity : IGameEntity
-    {
         if(entityID == -1)
         {
             outEntity = (TEntity)Activator.CreateInstance(typeof(TEntity), args:variadicArgs);
             m_loadedEntities.Add(outEntity);
-            m_loadedEntityIDs.Add(s_lastID++);
             m_loadedEntitiesActiveState.Add(true);
-            entityID = m_loadedEntityIDs[m_loadedEntityIDs.Count - 1];
+            return m_loadedEntities.Count - 1;
         }
         else
         {
             outEntity = (TEntity)Activator.CreateInstance(typeof(TEntity), args:variadicArgs);
-            m_loadedEntities[entityID] = outEntity; //m_loadedEntityIDs is the same
+            m_loadedEntities[entityID] = outEntity;
             m_loadedEntitiesActiveState[entityID] = true;
-            entityID = m_loadedEntityIDs[entityID];
+            return entityID;
         }
-
-        if(OnEntityAddedCallback != null)
-        {
-            OnEntityAddedCallback.Invoke(m_loadedEntities[entityID]);
-        }
-
-        return entityID;
     }
 
+    /*public EntityID CreatePersistentEntity<TEntity>(out TEntity outEntity, params object[] variadicArgs) where TEntity : IGameEntity*/
+    /*{*/
+    /*    int deletedEntityID = -1;*/
+    /**/
+    /*    for(int i = 0; i < persistentDataOffset; i++)*/
+    /*    {*/
+    /*        if(m_loadedEntitiesActiveState[i])*/
+    /*        {*/
+    /*            deletedEntityID = i;*/
+    /*            break;*/
+    /*        }*/
+    /*    }*/
+    /**/
+    /*    persistentDataOffset++;*/
+    /*    if(deletedEntityID == -1)*/
+    /*    {*/
+    /*        outEntity = (TEntity)Activator.CreateInstance(typeof(TEntity), args:variadicArgs);*/
+    /*        m_loadedEntities.Insert(persistentDataOffset, outEntity);*/
+    /*        m_loadedEntityIDs.Insert(persistentDataOffset, s_lastID++);*/
+    /*        m_loadedEntitiesActiveState.Insert(persistentDataOffset, true);*/
+    /*        return persistentDataOffset;*/
+    /*    }*/
+    /*    else*/
+    /*    {*/
+    /*        return CreateEntityWithID<TEntity>(deletedEntityID, out outEntity);*/
+    /*    }*/
+    /*}*/
+
+    //useless
+    /*private EntityID CreateEntity<TEntity>(out TEntity outEntity, params object[] variadicArgs) where TEntity : IGameEntity*/
+    /*{*/
+    /*    int deletedEntityIndex = m_loadedEntitiesActiveState.FindIndex((bool state) => { return state == false;});*/
+    /*    for(int i=0; i < m_loadedEntitiesActiveState.Count; i++)*/
+    /*    {*/
+    /*        if(i >= persistentDataOffset && m_loadedEntitiesActiveState[i])*/
+    /*        {*/
+    /*            deletedEntityIndex = i;*/
+    /*            break;*/
+    /*        }*/
+    /*    }*/
+    /**/
+    /*    return CreateEntityWithID<TEntity>(deletedEntityIndex, out outEntity, variadicArgs);*/
+    /*}*/
+    /**/
+    /*private EntityID CreateEntityWithID<TEntity>(int entityID, out TEntity outEntity, params object[] variadicArgs) where TEntity : IGameEntity*/
+    /*{*/
+    /*    if(entityID == -1)*/
+    /*    {*/
+    /*        outEntity = (TEntity)Activator.CreateInstance(typeof(TEntity), args:variadicArgs);*/
+    /*        m_loadedEntities.Add(outEntity);*/
+    /*        m_loadedEntityIDs.Add(s_lastID++);*/
+    /*        m_loadedEntitiesActiveState.Add(true);*/
+    /*        entityID = m_loadedEntityIDs[m_loadedEntityIDs.Count - 1];*/
+    /*    }*/
+    /*    else*/
+    /*    {*/
+    /*        outEntity = (TEntity)Activator.CreateInstance(typeof(TEntity), args:variadicArgs);*/
+    /*        m_loadedEntities[entityID] = outEntity; //m_loadedEntityIDs is the same*/
+    /*        m_loadedEntitiesActiveState[entityID] = true;*/
+    /*        entityID = m_loadedEntityIDs[entityID];*/
+    /*    }*/
+    /**/
+    /*    if(OnEntityAddedCallback != null)*/
+    /*    {*/
+    /*        OnEntityAddedCallback.Invoke(m_loadedEntities[entityID]);*/
+    /*    }*/
+    /**/
+    /*    return entityID;*/
+    /*}*/
+    /**/
     public bool IsEntityExisting(EntityID id)
     {
-        return m_loadedEntitiesActiveState.Count < id && m_loadedEntitiesActiveState[id];
+        return m_loadedEntitiesActiveState.Count > id && m_loadedEntitiesActiveState[id];
     }
-
-    public void DestroyEntity(EntityID id)
+    /**/
+    public void DestroyPersistentEntity(EntityID id)
     {
         if(IsEntityExisting(id))
         {
@@ -118,5 +146,29 @@ class EntityManager
         }
 
         return default;
+    }
+
+    public TGraphicsScript GenerateVisualInfos<TGraphicsScript>(GameEntityGraphics graphicsPrefab, Transform transform) where TGraphicsScript : GameEntityGraphics
+    {
+        TGraphicsScript result = UnityEngine.Object.Instantiate<TGraphicsScript>((TGraphicsScript)graphicsPrefab, transform);
+
+        if(OnVisualCreatedCallback != null)
+        {
+            OnVisualCreatedCallback.Invoke(result);
+        }
+
+        return result;
+    }
+
+    public GameObject GenerateVisualInfos(GameObject graphicsPrefab, Transform transform)
+    {
+        GameObject result = UnityEngine.Object.Instantiate(graphicsPrefab, transform) ;
+
+        if(OnVisualCreatedCallback != null)
+        {
+            OnVisualCreatedCallback.Invoke(null);
+        }
+
+        return result;
     }
 }
--- BattriKeepel2/Assets/Scripts/Systems/GameManager.cs
+++ BattriKeepel2/Assets/Scripts/Systems/GameManager.cs
@@ -8,11 +8,11 @@ public class GameManagerLogger : Logger
 class BossEntity : IGameEntity
 {
     BossGraphics entityGraphics;
-    int health = 100;
+    public int health = 100;
 
     public BossEntity(Transform spawnLocation, BossGraphics bossGrpahics)
     {
-        entityGraphics = Object.Instantiate<BossGraphics>(bossGrpahics, spawnLocation);
+        entityGraphics = EntityManager.Get().GenerateVisualInfos<BossGraphics>(bossGrpahics, spawnLocation);
     }
 
     public void TakeDamage(int amount)
@@ -26,12 +26,30 @@ class BossEntity : IGameEntity
 public class GameManager : GameEntityMonoBehaviour
 {
     BossEntity entity0;
+    BossEntity entity1;
+    BossEntity entity2;
     [SerializeField] Transform tr;
     [SerializeField] BossGraphics bg;
     int id;
+    int id1;
+    int id2;
     void Start()
     {
-        id = EntityManager.GetInstance().CreateEntity<BossEntity>(out entity0, tr, bg);
+        id = EntityManager.Get().CreatePersistentEntity<BossEntity>(out entity0, tr, bg);
         entity0.TakeDamage(25);
+        id1 = EntityManager.Get().CreatePersistentEntity<BossEntity>(out entity1, tr, bg);
+        id2 = EntityManager.Get().CreatePersistentEntity<BossEntity>(out entity2, tr, bg);
+
+        entity0.TakeDamage(25);
+        entity1.TakeDamage(25);
+
+        BossEntity get = EntityManager.Get().GetEntity<BossEntity>(id);
+        Log.Info<GameManagerLogger>("entity 0 hp : " + get.health);
+
+        BossEntity get1 = EntityManager.Get().GetEntity<BossEntity>(id1);
+        Log.Info<GameManagerLogger>("entity 1 hp : " + get1.health);
+
+        BossEntity get2 = EntityManager.Get().GetEntity<BossEntity>(id2);
+        Log.Info<GameManagerLogger>("entity 2 hp : " + get2.health);
     }
 }
