3d535ccad99b5f40d08a4ed2cbc34e065d65aa30
Maximilien <40921999+Maxodie@users.noreply.github.com>
2025-04-29 22:51:53 +0200

    feat : visual debugger logger base

--- BattriKeepel2/Assets/Scripts/Log/InGameLogger.cs
+++ BattriKeepel2/Assets/Scripts/Log/InGameLogger.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
 public class InGameLogger : MonoBehaviour
 {
-    [SerializeField] TMP_Text logText;
+#if DEVELOPMENT_BUILD || UNITY_EDITOR
+    [SerializeField] TMP_Text m_logText;
+    [SerializeField] int m_maxCharCount;
+    List<int> m_currentMsgLengths = new();
+#endif
 
     void Awake()
     {
@@ -15,7 +20,32 @@ public class InGameLogger : MonoBehaviour
 #if DEVELOPMENT_BUILD || UNITY_EDITOR
     public void SendLog(string text)
     {
-        logText.text += text + "\n\n";
+
+        string finalText = text + "\n";
+        if(finalText.Length >= m_maxCharCount)
+        {
+            Log.Warn<DebuggerLogger>("failed to draw a log, please make its Length less than " + m_maxCharCount);
+        }
+
+        while(m_logText.text.Length + finalText.Length > m_maxCharCount)
+        {
+            m_logText.text = m_logText.text.Remove(0, m_currentMsgLengths[0]);
+            UpdateLogMsgLength(finalText.Length);
+
+        }
+
+        m_logText.text += finalText;
+        m_currentMsgLengths.Add(finalText.Length);
+    }
+
+    void UpdateLogMsgLength(int logLength)
+    {
+        for(int i = 1; i < m_currentMsgLengths.Count; i++)
+        {
+            m_currentMsgLengths[i - 1] = m_currentMsgLengths[i];
+        }
+
+        m_currentMsgLengths[m_currentMsgLengths.Count - 1] = logLength;
     }
 #endif
 }
