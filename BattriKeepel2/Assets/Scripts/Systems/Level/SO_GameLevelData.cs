2681b65352524b5a1bd8a6b2f675521054990752
Maximilien <40921999+Maxodie@users.noreply.github.com>
2025-05-07 18:22:45 +0200

    feat : art data setup

--- BattriKeepel2/Assets/Scripts/Systems/Level/SO_GameLevelData.cs
+++ BattriKeepel2/Assets/Scripts/Systems/Level/SO_GameLevelData.cs
@@ -6,9 +6,3 @@ public class SO_GameLevelData : SO_LevelData
     public SO_LevelPhase[] levelPhases;
     public SO_LevelArtData levelArtData;
 }
-
-[CreateAssetMenu(fileName = "LevelArtData", menuName = "Level/LevelArtData")]
-public class SO_LevelArtData : ScriptableObject
-{
-    public GameObject gameArt;
-}
---
+++ BattriKeepel2/Assets/Scripts/Systems/Level/SO_LevelArtData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelArtData", menuName = "Level/LevelArtData")]
+public class SO_LevelArtData : ScriptableObject
+{
+    public GameEntityGraphics gameArt;
+}
