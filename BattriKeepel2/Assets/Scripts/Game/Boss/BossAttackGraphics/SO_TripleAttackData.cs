391e389b68bfc7812baa2e5d680cd1047dc0c6bc
Sasha <132772266+notpatern@users.noreply.github.com>
2025-06-23 10:12:33 +0200

    feat: keepel

--- BattriKeepel2/Assets/Scripts/Game/Boss/BossAttackGraphics/SO_TripleAttackData.cs
+++ BattriKeepel2/Assets/Scripts/Game/Boss/BossAttackGraphics/SO_TripleAttackData.cs
@@ -1,5 +1,12 @@
 using UnityEngine;
 
-public class SO_TripleAttackData : ScriptableObject {
+[CreateAssetMenu(fileName = "bossTripleAttack", menuName = "boss/bossTripleAttack")]
+public class SO_TripleAttackData : SO_BossAttackData {
     public SO_BulletData bulletData;
+    public float angle;
+
+    public override System.Type GetAttackType()
+    {
+        return typeof(TripleAttack);
+    }
 }
--- BattriKeepel2/Assets/Scripts/Game/Boss/BossAttackGraphics/TripleAttack.cs
+++ BattriKeepel2/Assets/Scripts/Game/Boss/BossAttackGraphics/TripleAttack.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using GameEntity;
 using UnityEngine;
 
 public class TripleAttack : BossAttackParent {
@@ -5,19 +7,65 @@ public class TripleAttack : BossAttackParent {
 
     GameObject origin;
     Vector2 bossPosition;
+    List<Bullet> m_bullets = new List<Bullet>();
+    SO_TripleAttackData m_data;
 
-    public void Init(Vector3 bossPosition, GameEntity.Player player) {
+    AttackGraphicsPool m_pool;
+    int bulletCount;
+
+    public override void Init(BossEntity boss, SO_BossAttackData data, AttackGraphicsPool graphicsPool, Player player)
+    {
         origin = new GameObject();
-        origin.transform.position = bossPosition;
-        this.bossPosition = bossPosition;
+        // origin.SetActive(true);
+        origin.transform.position = boss.GetGraphics().transform.position;
+        this.bossPosition = boss.GetGraphics().transform.position;
+        m_pool = graphicsPool;
+        m_data = (SO_TripleAttackData)data;
 
         float angle = Mathf.Atan2
             (player.position.y - bossPosition.y, player.position.x - bossPosition.x);
 
         angle = angle * Mathf.Rad2Deg;
+
+        PlaceBullets();
+
+        origin.transform.eulerAngles = new Vector3(angle, 0, 0);
     }
 
-    public void Update() {
+    private void PlaceBullets() {
+        for (int i = -1; i < 2; i++) {
+            float currentXEuleur = origin.transform.eulerAngles.x;
 
+            origin.transform.eulerAngles = new Vector3(currentXEuleur + m_data.angle * i, 0, 0);
+            m_bullets.Add(new Bullet(m_pool, m_data.bulletData, Vector3.down * 0.5f, origin.transform, true, Vector3.down, typeof(Player)));
+
+            origin.transform.eulerAngles = new Vector3(currentXEuleur, 0, 0);
+        }
+    }
+
+    public override void Update() {
+        bulletCount = m_bullets.Count;
+        for (int i = 0; i < m_bullets.Count; i++) {
+            if (m_bullets[i].IsDead()) {
+                bulletCount--;
+                continue;
+            }
+            m_bullets[i].Update();
+        }
+        if(bulletCount == 0)
+        {
+            Clean();
+        }
+    }
+
+    public override void Clean() {
+        foreach(Bullet bullet in m_bullets)
+        {
+            if(bullet.GetGraphics() != null)
+            {
+                m_pool.StopBullet(bullet.GetGraphics());
+            }
+        }
+        origin.gameObject.SetActive(false); // get out of this shit
     }
 }
